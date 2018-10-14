d8adf6dc99da8eddc076411353202ac7f21ef53b
Benjamin Streit <benjaminmstreit@gmail.com>
2018-10-13 19:51:42 -0500

    Completion of task number 7, link below.
    https://benja.orangescrum.com/dashboard#/details/fd5aa44a55585317b755d6d00161152b
    
    The new getSubdirectory() accepts a user_id and a subdirectory as
    arguments, and returns matches from mongodb in an array of document
    objects. The logic is very similar to getRootDirectory(), the find()
    query is simply modified to only find documents with metadata.path ==
    subdirectory parameter.

--- routes/FileInteraction.js
+++ routes/FileInteraction.js
@@ -17,7 +17,6 @@ class FileInteraction {
   /**
     * Retrieves an array of all documents within root directory of user collection
     * @param {String} user_id - the user id to retrieve root directory for
-    * @param {String} db - the db to use within mongoDB
     * @returns {Array} documents - an array of document objects
   */
   getRootDirectory(user_id) {
@@ -26,6 +25,24 @@ class FileInteraction {
       console.log("Connected correctly to server");
 
       db.collection(user_id + '.files').find().toArray(function(err, documents) {
+        db.close();
+        return documents;
+      });
+    });
+  }
+
+  /**
+    * Retrieves an array of all documents within a specific subdirectory of user collection
+    * @param {String} user_id - the user id to retrieve subdirectory for
+    * @param {String} subdirectory - the subdirectory to retrieve
+    * @returns {Array} documents - an array of document objects
+  */
+  getSubdirectory(user_id, subdirectory) {
+    mongodb.MongoClient.connect(this.url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+
+      db.collection(user_id + '.files').find({'metadata.path': subdirectory}).toArray(function(err, documents) {
         db.close();
         return documents;
       });
