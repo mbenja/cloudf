8136f40ac2079d47d285f77997239d2ea8862eef
Benjamin Streit <benjaminmstreit@gmail.com>
2018-11-20 18:20:17 -0600

    Implementing test for getRootDirectory.

--- public/js/test_suite.js
+++ public/js/test_suite.js
@@ -1,7 +1,30 @@
 /**
+  * Calls all necessary tests for testing each portion of application
+  * Test scores are updating accordingly
+*/
+function runTestSuite() {
+  testBackend();
+}
+
+/**
   * Calls all necessary tests for testing back-end
   * Test scores are updating accordingly
 */
 function testBackend() {
-  console.log("Here");
+  var result;
+
+  result = testGetRootDirectory();
+  console.log("getRootDirectory: " + result);
+}
+
+/**
+  * Testing getRootDirectory
+*/
+function testGetRootDirectory() {
+  refreshData();
+  if (current_file_data.length != 0) {
+    return "PASSED";
+  } else {
+    return "FAILED";
+  }
 }
